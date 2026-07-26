0a08f1b749041a5eb42e033eebca5226880a4876
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-07-26 11:19:41 +0300

    Fix Typo mistake in IEventStore GetStreamAsync method

--- LedgerFlow.Application/Common/Interfaces/IEventStore.cs
+++ LedgerFlow.Application/Common/Interfaces/IEventStore.cs
@@ -5,5 +5,5 @@ namespace LedgerFlow.Application.Common.Interfaces;
 public interface IEventStore
 {
     Task AppendAsync(Guid streamId, string streamType, int expectedVersion, IEnumerable<IDomainEvent> events);
-    Task<IEnumerable<IDomainEvent>> GetStreamASync(Guid id);
+    Task<IEnumerable<IDomainEvent>> GetStreamAsync(Guid id);
 }
