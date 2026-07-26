30f732440e22bed9f30b415bb472a1f11bae7eff
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-07-26 11:34:43 +0300

    Add commands for Deposit, Trade and Withdrawn in Application layer

---
+++ LedgerFlow.Application/Commands/RecordWithdrawnCommand.cs
@@ -0,0 +1,9 @@
+﻿using LedgerFlow.Domain.ValueObjects;
+using MediatR;
+
+namespace LedgerFlow.Application.Commands;
+
+public record RecordWithdrawnCommand(
+    Guid WalletId,
+    Money Money
+    ) : IRequest;
