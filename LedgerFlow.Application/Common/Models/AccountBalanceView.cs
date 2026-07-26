2a49e460137db450b29d88199c3426f21d17e1ae
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-07-26 19:36:24 +0300

    Add ReadModel data transfer object for table "AccountBalances"

---
+++ LedgerFlow.Application/Common/Models/AccountBalanceView.cs
@@ -0,0 +1,9 @@
+﻿namespace LedgerFlow.Application.Common.Models;
+
+public class AccountBalanceView
+{
+    public Guid WalletId { get; set; }
+    public string Currency { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+    public DateTime LastUpdatedAt { get; set; }
+}
