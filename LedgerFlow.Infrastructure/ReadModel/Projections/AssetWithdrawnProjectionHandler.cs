e62d681c66f905ce6f4652ca7a2a9c50abb389b8
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-07-26 19:37:20 +0300

    Add projection handlers for deposit, withdraw, and trade
    Added AssetDepositedProjectionHandler with balance update logic.
    Added AssetWithdrawnProjectionHandler and TradeExucutedProjectionHandler stubs.
    Included necessary using directives for MediatR and models.

---
+++ LedgerFlow.Infrastructure/ReadModel/Projections/AssetWithdrawnProjectionHandler.cs
@@ -0,0 +1,12 @@
+﻿using LedgerFlow.Application.Commands;
+using MediatR;
+
+namespace LedgerFlow.Infrastructure.ReadModel.Projections;
+
+public class AssetWithdrawnProjectionHandler : IRequestHandler<RecordWithdrawnCommand>
+{
+    public Task Handle(RecordWithdrawnCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
