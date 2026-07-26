1b0bf4a42ad5b654224428f815ab633c40c87502
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-07-26 11:35:12 +0300

    Add request handlers for added commands in Application layer

---
+++ LedgerFlow.Application/Handlers/RecordWithdrawnHandler.cs
@@ -0,0 +1,21 @@
+﻿using LedgerFlow.Application.Commands;
+using LedgerFlow.Application.Common.Interfaces;
+using LedgerFlow.Domain.Aggragates;
+using MediatR;
+
+namespace LedgerFlow.Application.Handlers;
+
+public class RecordWithdrawnHandler(IEventStore eventStore) : IRequestHandler<RecordWithdrawnCommand>
+{
+    public async Task Handle(RecordWithdrawnCommand request, CancellationToken cancellationToken)
+    {
+        var history = await eventStore.GetStreamAsync(request.WalletId);
+        var entry = LedgerEntry.Replay(history);
+
+        entry.ApplyWithdraw(request.Money);
+
+        await eventStore.AppendAsync(entry.Id, nameof(entry), entry.Version - entry.GetDomainEvents().Count, entry.GetDomainEvents());
+
+        entry.ClearUncommitedEvents();
+    }
+}
